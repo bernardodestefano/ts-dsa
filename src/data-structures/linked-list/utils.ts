4b852de46f899e688bcded5b4f47de3b33c9884f
Bernardo de Stefano <bernardo.destefano@gmail.com>
2022-05-14 16:35:44 +0200

    renamed Node into ListNode

--- src/data-structures/linked-list/utils.ts
+++ src/data-structures/linked-list/utils.ts
@@ -1,13 +1,13 @@
-import { Node } from '../../models';
+import { ListNode } from '../../models';
 
-export function createLinkedListFromArray<T>(arr: Node<T>[]) {
+export function createLinkedListFromArray<T>(arr: ListNode<T>[]) {
   if (!arr || arr.length === 0) {
     return null;
   }
-  const head = new Node(arr[0]);
+  const head = new ListNode(arr[0]);
   let prev = head;
   for (let i = 1; i < arr.length; i++) {
-    const node = new Node(arr[i]);
+    const node = new ListNode(arr[i]);
     prev.next = node;
     prev = node;
   }
